60f06f5b25a819c5b9b18b422fa0d263c0d84bd6
Rakesh <rakesh@rakesh-Inspiron-3521.(none)>
2015-02-07 00:16:30 +0530

    d

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,8 +2,8 @@ class ApiController < ApplicationController
   respond_to :json
   def patient
     @d_id = params[:d_id]
-   #@patients = Patient.where(:discharge_status => 0, :hospitaladmin_id => @d_id)
-     respond_with(@d_id)
+    @patients = Patient.where(:discharge_status => 0, :physician => @d_id)
+     respond_with(@patients)
   end
     def disease
    @diseasecode = Diseasecode.all
